e603a4f403c99a80d08132272342cc4b16e2443a
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-26 10:20:31 -0500

    app js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from  "./pages/Home";
+import Contact from  "./pages/Contact";
+import Education from "./pages/Education";
+import Hobbies from "./pages/Hobbies";
+import About from  "./pages/About";
+import { Route , Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div>  
+ <Routes>
+ <Route path="/" element={<MainLayout />} >
+    <Route index element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route path="education" element={<Education />} />
+    <Route path="hobbies" element={<Hobbies />} />
+    <Route path="contact" element={<Contact />} /> 
+</Route>
+<Route path="*" element={<p>Invalid URL</p>} />
+    </Routes>
+</div>);
 }
 
 export default App;
