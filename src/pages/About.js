fa391656bfbdc62f4bd90894e127f4af92d3eaa6
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-26 10:17:59 -0500

    about page updated

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h1>About Me</h1>
+        <h3>
+        Hi I'm Nupur Kesarwani.I was born in India and received 
+        my B.Sc. and M.Sc from University of Allahabad in Computer Science. 
+        Currently, I'm a pursuing Full Stack Developer Course from John Abott College  .
+        </h3>
+        </div>
+  )
+}
