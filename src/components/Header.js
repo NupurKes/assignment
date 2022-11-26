33ae44a9f91e3ab43254462d74268ee4f4118751
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-26 10:18:55 -0500

    Footer updated

---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Header.css";
+import HomeButton from "./HomeButton";
+import { NavLink } from "react-router-dom";
+
+
+
+export default function Header() {
+  const buttonStyle = {
+    backgroundColor: "green",
+    border: "none",
+    color: "white",
+    padding: "15px 32px",
+    textAlign: "center",
+    textDecoration: "none",
+    display: "inline-block",
+    fontSize: "16px",
+    };
+    
+  return (
+    <div>
+      <div>
+        <HomeButton />
+      </div>
+      <div>
+
+        <NavLink className={({ isActive }) =>
+           isActive ? "link-active" : "link-inactive" }
+           to="/" > Home
+           
+        </NavLink>
+
+        
+        <NavLink className={({ isActive }) =>
+           isActive ? "link-active" : "link-inactive" }
+           to="/about" > About
+           
+        </NavLink>
+
+        <NavLink className={({ isActive }) =>
+           isActive ? "link-active" : "link-inactive" }
+           to="/contact" > Contact
+        </NavLink>
+
+        <NavLink to ="/">
+      <button style={buttonStyle}>
+      {" "}
+      <p>Home</p>
+    </button>
+    </NavLink>
+      </div>
+    </div>
+  );
+}
