2e005a51dea1fabe02051770c4d5f3aab6846114
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-26 10:16:26 -0500

    about js updated

---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+        <h1>Get In Touch</h1>
+         <h2>My Contact</h2>
+         <h3>Feel free to contact me 24/7. You can either send me a message @  514-222-2222 or send me an email @ nupur@gmail.com.</h3>
+
+    </div>
+  )
+}
---
+++ src/pages/Education.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Education() {
+  return (
+    <div>
+        <h1>My Education</h1>
+        <ul>
+             <li>MASTER DEGREE IN SOFTWARE ENGINEERING</li>
+             <li>BACHELOR DEGREE IN SOFTWARE ENGINEERING</li>
+             <li>HIGH SCHOOL DIPLOMA IN MATHEMATICS AND PHYSICS </li>
+        </ul>
+  
+        </div>
+  )
+}
